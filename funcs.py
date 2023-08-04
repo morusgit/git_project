ee9d8d013385797a21876f596014f98412a65171
morusgit <morus@bk.ru>
2023-08-04 23:03:21 +0300

    imported sum in main

---
+++ funcs.py
@@ -0,0 +1,7 @@
+def sum_2_numbers(num1, num2):
+    sum = (num1+num2)
+    return sum
+
+def sub_2_numbers(num1, num2):
+    sub = (num1-num2)
+    return sub
---
+++ main.py
@@ -0,0 +1,1 @@
+from funcs import sum_2_numbers
